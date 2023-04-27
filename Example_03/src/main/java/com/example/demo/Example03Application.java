d009147fd5da38ef105e2b4dccfa3d00eb32aa5d
Graziano <graziano.aloisi@gmail.com>
2023-04-27 15:16:34 +0200

    agiunto commento

--- Example_03/src/main/java/com/example/demo/Example03Application.java
+++ Example_03/src/main/java/com/example/demo/Example03Application.java
@@ -9,6 +9,7 @@ public class Example03Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Example03Application.class, args);
 		System.out.println("Modifica_01");
+		//===Per configurare
 		System.out.println("Modifica_02");
 	}
 
